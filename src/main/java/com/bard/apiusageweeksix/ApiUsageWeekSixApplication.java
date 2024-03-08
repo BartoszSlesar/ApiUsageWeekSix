9e873496b2fc8c4d9f75d824888ff7d1158a05f4
bard <bartoszslesar@gmail.com>
2024-03-08 21:20:45 +0100

    project initialization

---
+++ src/main/java/com/bard/apiusageweeksix/ApiUsageWeekSixApplication.java
@@ -0,0 +1,13 @@
+package com.bard.apiusageweeksix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsageWeekSixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUsageWeekSixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bard/apiusageweeksix/ApiUsageWeekSixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bard.apiusageweeksix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsageWeekSixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
