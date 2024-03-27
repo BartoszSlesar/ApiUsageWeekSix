97503bd5f0593f851ed776289761d12bcc17e734
bard <bartoszslesar@gmail.com>
2024-03-27 20:22:51 +0100

    changed RestTemplate to RestClient

--- src/main/java/com/bard/apiusageweeksix/services/SpellService.java
+++ src/main/java/com/bard/apiusageweeksix/services/SpellService.java
@@ -4,25 +4,27 @@ package com.bard.apiusageweeksix.services;
 import com.bard.apiusageweeksix.config.ApiConfiguration;
 import com.bard.apiusageweeksix.model.Spell;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class SpellService {
-    private RestTemplate restTemplate;
+    private RestClient restClient;
     private final ApiConfiguration apiConfiguration;
 
     public SpellService(ApiConfiguration apiConfiguration){
         this.apiConfiguration = apiConfiguration;
-        this.restTemplate = new RestTemplate();
+        this.restClient = RestClient.create();
     }
 
     public List<Spell> getAllSpells(){
         String callUrl = apiConfiguration.getSpellApiUrl();
-        Spell[] spellData = restTemplate.getForObject(
-                callUrl, Spell[].class);
+        Spell[] spellData = restClient.get()
+                .uri(callUrl)
+                .retrieve()
+                .body(Spell[].class);
 
         return Arrays.asList(spellData);
     }
